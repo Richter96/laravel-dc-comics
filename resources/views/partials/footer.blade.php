e219b9e3a4a47d83ec200df7021671864078bc62
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-31 16:16:01 +0200

    add footer and header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,66 @@
-<h2>footer</h2>
+<footer id="app_footer">
+    <section class="top_footer">
+        <div class="row row-cols-6 container-md m-auto">
+            <div class="col d-flex flex-column">
+                <h5>DC COMICS</h5>
+                <ul>
+                    <li><a href="">Characters</a> </li>
+                    <li><a href="">Comics</a> </li>
+                    <li><a href="">Movie</a> </li>
+                    <li><a href="">Tv</a> </li>
+                    <li><a href="">Games</a> </li>
+                    <li><a href="">Videos</a> </li>
+                    <li><a href="">News</a> </li>
+                </ul>
+                <h5>SHOP</h5>
+                <ul>
+                    <li><a href="">Shop Dc</a> </li>
+                    <li><a href="">Shop Dc Collectibles</a> </li>
+                </ul>
+            </div>
+            <div class="col d-flex flex-column">
+                <h5>DC</h5>
+                <ul>
+                    <li><a href=""> Terms Of Use</a> </li>
+                    <li><a href=""> Privacy policy (New)</a> </li>
+                    <li><a href=""> Ad Choices</a> </li>
+                    <li><a href=""> Advertising</a> </li>
+                    <li><a href=""> Jobs</a> </li>
+                    <li><a href=""> Subscriptions</a> </li>
+                    <li><a href=""> Talent Workshops</a></li>
+                    <li><a href=""> CPSC Certificates</a></li>
+                    <li><a href=""> Ratings</a></li>
+                    <li><a href=""> Shop Help</a></li>
+                    <li><a href=""> Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col d-flex flex-column">
+                <h5>SITES</h5>
+                <ul>
+                    <li><a href="">Dc</a></li>
+                    <li><a href="">MAD Magazine</a></li>
+                    <li><a href="">Dc Kids</a></li>
+                    <li><a href="">DC Universe</a></li>
+                    <li><a href="">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="bottom_footer">
+        <div class="row container-md m-auto">
+            <div class="col d-flex align-items-center">
+                <button class="btn_singup bg-transparent">SING-UP NOW!</button>
+            </div>
+            <div class="social col d-flex align-items-center justify-content-end">
+                <span class="fw-bold">FOLLOW AS</span>
+                <img class="ps-3" src="Vite::asset('resources/img/footer-facebook')}}
+                "
+                    alt="">
+                <img class="ps-3" src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                <img class="ps-3" src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                <img class="ps-3" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                <img class="ps-3" src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </section>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,44 @@
-<h2>header</h2>
+<header id="app_header">
+    <div class="container-md d-flex align-items-center justify-content-between">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <nav class="nav justify-content-center">
+            <a class="nav-link {{ Route::currentRouteName() === 'home' ? 'active' : '' }}" href="{{ route('home') }}"
+                aria-current="page">HOME</a>
+            <a class="nav-link {{ Route::currentRouteName() === 'comic' ? 'active' : '' }} " href="{{ route('comic') }}"
+                aria-current="page">COMICS</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">MOVIES</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">TV</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">GAMES</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">COLLECTIBLES</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">VIDEOS</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">FANS</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">NEWS</a>
+            <a class="nav-link {{ Route::currentRouteName() === '#' ? 'active' : '' }}" href="#"
+                aria-current="page">SHOP</a>
+        </nav>
+    </div>
+</header>
+
+<style>
+    nav a {
+        color: #0c7cec;
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        border-bottom: 3px solid #0c7cec;
+    }
+
+    nav a:not(.active) {
+        border-bottom: none;
+        color: black;
+
+    }
+</style>
