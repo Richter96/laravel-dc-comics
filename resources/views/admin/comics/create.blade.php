50436f6e5d9eb54dd4f5b6082fe795d1b0e9e520
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-01 16:09:35 +0200

    add validation error and edit formatter sintax

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -1,105 +1,89 @@
 @extends('layouts.app')
 
 @section('title')
-    CREATE NEW COMICS
+CREATE NEW COMICS
 @endsection
 
 @section('content')
-    <div class="container">
-        @if ($errors->any())
-            <div class="alert alert-danger" roule="alert">
-                <h5><strong>Ci sono alcuni errori:</strong></h5>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+<div class="container">
+    @include('partials.validation_errors')
+    <form action="{{ route('comics.store') }}" method="post">
+        @csrf
+        {{-- title --}}
+        <div class="mb-3">
+            <label for="title" class="form-label">TITOLO</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" aria-describedby="helpId" placeholder="Inserisci un titolo" value="{{ old('title') }}">
+            <small id="helpId" class="form-text text-muted">max 100 charatteri</small>
+            @error('name')
+            <div class="alert alert-danger" role="alert">
+                <strong>Name, Error:</strong>{{ $message }}
             </div>
-        @endif
-        <form action="{{ route('comics.store') }}" method="post">
-            @csrf
-            {{-- title --}}
-            <div class="mb-3">
-                <label for="title" class="form-label">TITOLO</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
-                    aria-describedby="helpId" placeholder="Inserisci un titolo" value="{{ old('title') }}">
-                <small id="helpId" class="form-text text-muted">max 100 charatteri</small>
-                @error('name')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Name, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
+            @enderror
+        </div>
+        {{-- img --}}
+        <div class="mb-3">
+            <label for="thumb" class="form-label">IMMGAIN</label>
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" id="thumb" aria-describedby="helpId" placeholder="http://" value="{{ old('thumb') }}">
+            <small id="helpId" class="form-text text-muted">inserire url dell'immagine</small>
+            @error('thumb')
+            <div class="alert alert-danger" role="alert">
+                <strong>Image, Error:</strong>{{ $message }}
             </div>
-            {{-- img --}}
-            <div class="mb-3">
-                <label for="thumb" class="form-label">IMMGAIN</label>
-                <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb"
-                    id="thumb" aria-describedby="helpId" placeholder="http://" value="{{ old('thumb') }}">
-                <small id="helpId" class="form-text text-muted">inserire url dell'immagine</small>
-                @error('thumb')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Image, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
+            @enderror
+        </div>
+        {{-- type --}}
+        <div class="mb-3">
+            <label for="type" class="form-label">TIPOLOGIA</label>
+            <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type" aria-describedby="helpId" placeholder="" value="{{ old('type') }}">
+            <small id="helpId" class="form-text text-muted">max 50 charatteri</small>
+            @error('type')
+            <div class="alert alert-danger" role="alert">
+                <strong>Type, Error:</strong>{{ $message }}
             </div>
-            {{-- type --}}
-            <div class="mb-3">
-                <label for="type" class="form-label">TIPOLOGIA</label>
-                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type"
-                    aria-describedby="helpId" placeholder="" value="{{ old('type') }}">
-                <small id="helpId" class="form-text text-muted">max 50 charatteri</small>
-                @error('type')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Type, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
+            @enderror
+        </div>
+        {{-- description --}}
+        <div class="mb-3">
+            <label for="description" class="form-label">DESCRIZIONE</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="5" placeholder="Inserisci qui la descrizione">{{ old('description') }}</textarea>
+            @error('description')
+            <div class="alert alert-danger" role="alert">
+                <strong>Description, Error:</strong>{{ $message }}
             </div>
-            {{-- description --}}
-            <div class="mb-3">
-                <label for="description" class="form-label">DESCRIZIONE</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
-                    rows="5" placeholder="Inserisci qui la descrizione">{{ old('description') }}</textarea>
-                @error('description')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Description, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
+            @enderror
+        </div>
+        {{-- DATE --}}
+        <div class="mb-3">
+            <label for="sale_date" class="form-label">DATA</label>
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" id="sale_date" aria-describedby="helpId" placeholder="" value="{{ old('sale_date') }}">
+            <small id="helpId" class="form-text text-muted">dd-mm-yyyy</small>
+            @error('sale_date')
+            <div class="alert alert-danger" role="alert">
+                <strong>Date, Error:</strong>{{ $message }}
             </div>
-            {{-- DATE --}}
-            <div class="mb-3">
-                <label for="sale_date" class="form-label">DATA</label>
-                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date"
-                    id="sale_date" aria-describedby="helpId" placeholder="" value="{{ old('sale_date') }}">
-                <small id="helpId" class="form-text text-muted">dd-mm-yyyy</small>
-                @error('sale_date')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Date, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
+            @enderror
+        </div>
+        {{-- price --}}
+        <div class="mb-3">
+            <label for="price" class="form-label">PREZZO</label>
+            <input type="number" step="0.1" class="form-control @error('price') is-invalid @enderror" name="price" id="price" aria-describedby="helpId" placeholder="999,99" value="{{ old('price') }}">
+            <small id="helpId" class="form-text text-muted">prezzo max 999,99</small>
+            @error('price')
+            <div class="alert alert-danger" role="alert">
+                <strong>Price, Error:</strong>{{ $message }}
             </div>
-            {{-- price --}}
-            <div class="mb-3">
-                <label for="price" class="form-label">PREZZO</label>
-                <input type="number" step="0.1" class="form-control @error('price') is-invalid @enderror"
-                    name="price" id="price" aria-describedby="helpId" placeholder="999,99"
-                    value="{{ old('price') }}">
-                <small id="helpId" class="form-text text-muted">prezzo max 999,99</small>
-                @error('price')
-                    <div class="alert alert-danger" role="alert">
-                        <strong>Price, Error:</strong>{{ $message }}
-                    </div>
-                @enderror
-            </div>
+            @enderror
+        </div>
 
-            <div class="row">
-                <div class="col">
-                    <button type="submit" class="btn btn-success m-1">UPLOAD</button>
-                    <button type="reset" class="btn btn-danger">Reset</button>
-                </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-success m-1">UPLOAD</button>
+                <button type="reset" class="btn btn-danger">Reset</button>
             </div>
+        </div>
 
 
 
-        </form>
-    </div>
+    </form>
+</div>
 @endsection
--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -1,121 +1,110 @@
 @extends('layouts.app')
 
 @section('titole')
-    COMIX
+COMIX
 @endsection
 
 @section('content')
-    <div class=" container-xl">
-        @if (session('message'))
-            <div class="alert alert-success" role="alert">
-                <strong>{{ session('message') }}</strong>
-            </div>
-        @endif
+<div class=" container-xl">
+    @if (session('message'))
+    <div class="alert alert-success" role="alert">
+        <strong>{{ session('message') }}</strong>
+    </div>
+    @endif
 
-        <a class="btn btn-primary" href="{{ route('comics.create') }}" role="button">New</a>
+    <a class="btn btn-primary" href="{{ route('comics.create') }}" role="button">New</a>
 
-        <div class="table-responsive-xxl">
-            <table class="table table-hover table-bordered table-dark ">
-                <thead>
-                    <caption>COMICS ADMIN</caption>
-                    <tr class="text-center">
-                        <th>ID</th>
-                        <th>IMMAGINE</th>
-                        <th>TITOLO</th>
-                        <th>DESCRIZIONE</th>
-                        <th>PREZZO</th>
-                        <th class=" px-4">DATA DI RILASCIO</th>
-                        <th>TIPO</th>
-                        <th class="col-2">ACTION</th>
-                    </tr>
-                </thead>
-                <tbody class="table-group-divider">
-                    @forelse($comics as $comic)
-                        <tr class="">
-                            <td scope="row">{{ $comic->id }}</td>
-                            <td><img class="card-img-top" src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></td>
-                            <td>{{ $comic->title }}</td>
-                            <td>{{ $comic->description }}</td>
-                            <td>{{ $comic->price }}</td>
-                            <td>{{ $comic->sale_date }}</td>
-                            <td>{{ $comic->type }}</td>
-                            <td class="">
-                                <div class="row row-cols-3">
+    <div class="table-responsive-xxl">
+        <table class="table table-hover table-bordered table-dark ">
+            <thead>
+                <caption>COMICS ADMIN</caption>
+                <tr class="text-center">
+                    <th>ID</th>
+                    <th>IMMAGINE</th>
+                    <th>TITOLO</th>
+                    <th>DESCRIZIONE</th>
+                    <th>PREZZO</th>
+                    <th class=" px-4">DATA DI RILASCIO</th>
+                    <th>TIPO</th>
+                    <th class="col-2">ACTION</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @forelse($comics as $comic)
+                <tr class="">
+                    <td scope="row">{{ $comic->id }}</td>
+                    <td><img class="card-img-top" src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></td>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->description }}</td>
+                    <td>${{ $comic->price }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                    <td>{{ $comic->type }}</td>
+                    <td class="">
+                        <div class="row row-cols-3">
 
-                                    <div class="icon d-inline">
-                                        <a name="" id="" class="btn btn-primary w-1rem"
-                                            href="{{ Route('comics.show', $comic->id) }}" role="button"><i
-                                                class="fas fa-eye fa-sm fa-fw"></i></a>
-                                    </div>
+                            <div class="icon d-inline">
+                                <a name="" id="" class="btn btn-primary w-1rem" href="{{ Route('comics.show', $comic->id) }}" role="button"><i class="fas fa-eye fa-sm fa-fw"></i></a>
+                            </div>
 
-                                    <div class="icon d-inline">
-                                        <a name="" id="" class="btn btn-primary w-1rem"
-                                            href="{{ Route('comics.edit', $comic->id) }}" role="button"><i
-                                                class="fas fa-pencil fa-sm fa-fw"></i></a>
-                                    </div>
+                            <div class="icon d-inline">
+                                <a name="" id="" class="btn btn-primary w-1rem" href="{{ Route('comics.edit', $comic->id) }}" role="button"><i class="fas fa-pencil fa-sm fa-fw"></i></a>
+                            </div>
 
-                                    <div class="icon d-inline">
-                                        <!-- Modal trigger button -->
-                                        <button type="button" class="btn btn-danger btn-lg ratio-1x1 w-1rem p-0"
-                                            data-bs-toggle="modal" data-bs-target="#modal-{{ $comic->id }}">
-                                            <i class="fas fa-trash fa-sm fa-fw w-50"></i>
-                                        </button>
-                                        <!-- Modal Body -->
-                                        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                        <div class="modal fade" id="modal-{{ $comic->id }}" tabindex="-1"
-                                            data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                            aria-labelledby="modal-{{ $comic->id }}" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                                role="document">
-                                                <div class="modal-content">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title" id="modal-{{ $comic->id }}">DELATE
-                                                            {{ $comic->title }}</h5>
-                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                            aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="modal-body">
-                                                        Are you sure you want delete this element?
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary"
-                                                            data-bs-dismiss="modal">Close</button>
-                                                        <form action="{{ route('comics.destroy', $comic->id) }}"
-                                                            method="post">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-danger">yes!</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
+                            <div class="icon d-inline">
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-danger btn-lg ratio-1x1 w-1rem p-0" data-bs-toggle="modal" data-bs-target="#modal-{{ $comic->id }}">
+                                    <i class="fas fa-trash fa-sm fa-fw w-50"></i>
+                                </button>
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="modal-{{ $comic->id }}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modal-{{ $comic->id }}" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modal-{{ $comic->id }}">DELATE
+                                                    {{ $comic->title }}</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Are you sure you want delete this element?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">yes!</button>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
 
 
-                            </td>
+                    </td>
 
-                        </tr>
-                    @empty
-                        <tr>
-                            <td scope="roe">NON CI SONO FUMETTI</td>
-                        </tr>
-                    @endforelse
-
-
-
-                </tbody>
-                <tfoot>
-
-                </tfoot>
-            </table>
-        </div>
+                </tr>
+                @empty
+                <tr>
+                    <td scope="roe">NON CI SONO FUMETTI</td>
+                </tr>
+                @endforelse
 
 
-        {{-- <div class="row">
+
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+        </table>
+    </div>
+
+
+    {{-- <div class="row">
             
 
         </div> --}}
-    </div>
+</div>
 @endsection
